a932100ded81a8a7589bf17e42d2524eeeae500f
Sammy Fung <sammy@sammy.hk>
2015-03-20 17:37:57 +0800

    First commit

---
+++ data_gov_hk/data_gov_hk/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class DataGovHkItem(scrapy.Item):
+  datasetTitle = scrapy.Field()
+  resourceName = scrapy.Field()
+  resourceFormat = scrapy.Field()
+  resourceURL = scrapy.Field()
+  sourceUrl = scrapy.Field()
+  provider = scrapy.Field()
+  category = scrapy.Field()
+  updateFreq = scrapy.Field()
---
+++ data_gov_hk/data_gov_hk/spiders/data_govhk.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# DATA.GOV.HK Datasets Scraper
+# Sammy Fung <sammy@sammy.hk>
+import scrapy
+from data_gov_hk.items import DataGovHkItem
+
+class DataGovhkSpider(scrapy.Spider):
+  name = "data_govhk"
+  allowed_domains = ["data.gov.hk"]
+  start_urls = (
+    'http://www.data.gov.hk/',
+  )
+
+  def parse(self, response):
+    for i in response.xpath("//h3/a[contains(@class, 'media-view')]/@href").extract():
+      yield scrapy.Request("http://www.data.gov.hk%s"%i, callback=self.parse_category)
+
+  def parse_category(self, response):
+    for i in response.xpath("//h3[contains(@class, 'dataset-heading')]/a/@href").extract():
+      yield scrapy.Request("http://www.data.gov.hk%s"%i, callback=self.parse_dataset)
+
+  def parse_dataset(self, response):
+    title = response.xpath("//h3[contains(@id, 'resourceModalLabel')]/text()").extract()[0]
+    provider = response.xpath("//img[contains(@class, 'media-image')]/@alt").extract()
+    category = response.xpath("//a[contains(@class, 'badge')]/text()").extract()
+    updateFreq = response.xpath("//section[contains(@class, 'additional-info')]/table/tbody/tr/td/text()").extract()[1]
+    resource_name = response.xpath("//div[contains(@class, 'dataset-resource-name')]/div/div/a/@title").extract()
+    resource_format = response.xpath("//div[contains(@class, 'dataset-resource-format')]/span/text()").extract() 
+    resource_download = response.xpath("//div[contains(@class, 'dataset-resource-download')]/a/@href").extract()
+    for i in range(0, len(resource_name)):
+      item = DataGovHkItem()
+      item['datasetTitle'] = title
+      item['resourceName'] = resource_name[i]
+      item['resourceFormat'] = resource_format[i]
+      item['resourceURL'] = resource_download[i]
+      item['sourceUrl'] = response.url
+      item['provider'] = provider
+      item['category'] = category
+      item['updateFreq'] = updateFreq
+      yield item
+
